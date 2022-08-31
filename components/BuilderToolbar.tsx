fdd451d3dfa4fd74bb15d836744dc56a98525d57
Doug Perez <perezcpt@gmail.com>
2022-08-30 17:13:05 -0700

    native base added to project

--- components/BuilderToolbar.tsx
+++ components/BuilderToolbar.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {View} from 'react-native';
-import {Button, AppBar, IconButton, HStack} from '@react-native-material/core';
+import {AppBar, IconButton, HStack} from '@react-native-material/core';
 // import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 // import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -8,6 +8,7 @@ import Icon from 'react-native-vector-icons/Feather';
 import IconIon from 'react-native-vector-icons/Ionicons';
 import IconFoundation from 'react-native-vector-icons/Foundation';
 import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Popover, Button, Box, Center, NativeBaseProvider} from 'native-base';
 
 import {
   Provider,
@@ -32,6 +33,34 @@ const BuilderToolbar = () => {
       )}
       trailing={props => (
         <HStack>
+          <NativeBaseProvider>
+            <Popover
+              trigger={triggerProps => {
+                return (
+                  <Button {...triggerProps} colorScheme="danger">
+                    Delete Customer
+                  </Button>
+                );
+              }}>
+              <Popover.Content accessibilityLabel="Delete Customerd" w="56">
+                <Popover.Arrow />
+                <Popover.CloseButton />
+                <Popover.Header>Delete Customer</Popover.Header>
+                <Popover.Body>
+                  This will remove all data relating to Alex. This action cannot
+                  be reversed. Deleted data can not be recovered.
+                </Popover.Body>
+                <Popover.Footer justifyContent="flex-end">
+                  <Button.Group space={2}>
+                    <Button colorScheme="coolGray" variant="ghost">
+                      Cancel
+                    </Button>
+                    <Button colorScheme="danger">Delete</Button>
+                  </Button.Group>
+                </Popover.Footer>
+              </Popover.Content>
+            </Popover>
+          </NativeBaseProvider>
           <IconButton
             icon={props => <IconIon name="hammer" {...props} />}
             {...props}
