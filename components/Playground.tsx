c15cf65e1657dcd2ade20b5f32af4986352e45ea
Doug Perez <perezcpt@gmail.com>
2022-09-03 14:15:54 -0700

    react native to web coms test pass

--- components/Playground.tsx
+++ components/Playground.tsx
@@ -3,12 +3,19 @@ import {WebView} from 'react-native-webview';
 import {View} from 'react-native';
 
 const TaskToolbar = () => {
+  //   const comsTest = `
+  //   document.body.style.backgroundColor = 'red';
+  //   setTimeout(function() { window.alert('hi') }, 2000);
+  //   true; // note: this is required, or you'll sometimes get silent failures
+  // `;
   return (
     <View style={{height: 1000, top: -160}}>
       <WebView
         source={{uri: 'https://brickbuilder.herokuapp.com/'}}
         // https://nicmosc.github.io/brick-builder/
         style={{flex: 1}}
+        // onMessage={event => {}}
+        // injectedJavaScript={comsTest}
       />
     </View>
   );
