bd2c0da4e22a9acbbab4eafacbd342452ef8b3eb
Doug Perez <perezcpt@gmail.com>
2022-09-05 16:42:05 -0700

    wip

--- components/Playground.tsx
+++ components/Playground.tsx
@@ -3,19 +3,26 @@ import {WebView} from 'react-native-webview';
 import {View} from 'react-native';
 
 const TaskToolbar = () => {
-  //   const comsTest = `
-  //   document.body.style.backgroundColor = 'red';
-  //   setTimeout(function() { window.alert('hi') }, 2000);
-  //   true; // note: this is required, or you'll sometimes get silent failures
-  // `;
+  const WebAPICode = `test()`;
+  const comsTest = `
+    document.body.style.backgroundColor = 'red';
+    setTimeout(function() { window.alert('hi') }, 500);
+    true; // note: this is required, or you'll sometimes get silent failures
+  `;
+  // const injectedJavascript = `(function() {
+  //   window.postMessage = function(data) {
+  //     window.ReactNativeWebView.postMessage(data);
+  //   };
+  // })()`;
   return (
     <View style={{height: 1000, top: -160}}>
       <WebView
-        source={{uri: 'https://brickbuilder.herokuapp.com/'}}
-        // https://nicmosc.github.io/brick-builder/
+        source={{uri: 'https://dougwperez.github.io/brick-playground/'}}
+        // https://dougwperez.github.io/brick-playground/
+        // https://brickbuilder.herokuapp.com/
         style={{flex: 1}}
-        // onMessage={event => {}}
-        // injectedJavaScript={comsTest}
+        onMessage={event => {}}
+        injectedJavaScript={WebAPICode}
       />
     </View>
   );
