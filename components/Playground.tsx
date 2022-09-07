4184c56f829f0e31dd1d253f9edf51fc859c1cf9
Doug Perez <perezcpt@gmail.com>
2022-09-07 13:18:36 -0700

    major advancement

--- components/Playground.tsx
+++ components/Playground.tsx
@@ -2,20 +2,14 @@ import React from 'react';
 import {WebView} from 'react-native-webview';
 import {View} from 'react-native';
 
-const TaskToolbar = () => {
-  const WebAPICode = `test()`;
-  const comsTest = `
-    document.body.style.backgroundColor = 'red';
-    setTimeout(function() { window.alert('hi') }, 500);
-    true; // note: this is required, or you'll sometimes get silent failures
-  `;
-  // const injectedJavascript = `(function() {
-  //   window.postMessage = function(data) {
-  //     window.ReactNativeWebView.postMessage(data);
-  //   };
-  // })()`;
+const Playground = () => {
+  const WebAPICode = `
+  setTimeout(function() { document.querySelector(".ion-paintbrush").click(); }, 1000);
+  true`;
+
   return (
-    <View style={{height: 1000, top: -160}}>
+    // <View style={{height: 1000, top: -160}}>
+    <View style={{height: 1000, top: -10, right: 20}}>
       <WebView
         source={{uri: 'https://dougwperez.github.io/brick-playground/'}}
         // https://dougwperez.github.io/brick-playground/
@@ -23,8 +17,11 @@ const TaskToolbar = () => {
         style={{flex: 1}}
         onMessage={event => {}}
         injectedJavaScript={WebAPICode}
+        //       injectedJavaScriptBeforeContentLoaded={`
+        //       document.querySelector('.button__button--3UeOv').click();
+        // `}
       />
     </View>
   );
 };
-export default TaskToolbar;
+export default Playground;
