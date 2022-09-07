e0325e25db35e9d5992ef006645eac5405c1630d
Doug Perez <perezcpt@gmail.com>
2022-09-07 16:11:05 -0700

    more work done

--- components/Playground.tsx
+++ components/Playground.tsx
@@ -3,22 +3,21 @@ import {WebView} from 'react-native-webview';
 import {View} from 'react-native';
 
 const Playground = () => {
-  const WebAPICode = `
-  setTimeout(function() { document.querySelector(".ion-paintbrush").click(); }, 1000);
-  setTimeout(function() { document.querySelector(".ion-grid").click(); }, 3000);
-  setTimeout(function() { document.querySelector(".ion-grid").click(); }, 6000);
+  const toggleGrid = `
+  setTimeout(function() { document.querySelector(".ion-grid").click(); }, 1);
+  setTimeout(function() { document.querySelector(".ion-grid").click(); }, 2000);
   true`;
 
   return (
-    // <View style={{height: 1000, top: -160}}>
-    <View style={{height: 1000, top: -10, right: 20}}>
+    <View style={{height: 1000, top: -160}}>
+      {/* // <View style={{height: 1000, top: -10, right: 20}}> */}
       <WebView
         source={{uri: 'https://dougwperez.github.io/brick-playground/'}}
         // https://dougwperez.github.io/brick-playground/
         // https://brickbuilder.herokuapp.com/
         style={{flex: 1}}
         onMessage={event => {}}
-        injectedJavaScript={WebAPICode}
+        injectedJavaScript={toggleGrid}
         //       injectedJavaScriptBeforeContentLoaded={`
         //       document.querySelector('.button__button--3UeOv').click();
         // `}
