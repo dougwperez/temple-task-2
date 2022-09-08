4a41fee9ef987efc5e9ad23970d50025f121310d
Doug Perez <perezcpt@gmail.com>
2022-09-07 22:15:25 -0700

    save code

--- App.tsx
+++ App.tsx
@@ -30,32 +30,6 @@ import TaskToolbar from './components/BottomBar/TaskToolbar';
 import BuilderToolbar from './components/TopBar/BuilderToolbar';
 import Playground from './components/Playground';
 
-// const Section = (children: any, title: string): Node => {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
-
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const [gridStatus, setGridStatus] = useState(true);
