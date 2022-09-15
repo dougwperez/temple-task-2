489874d70b4e2bdf261a1ce73a6eb1316fff20a5
Doug Perez <perezcpt@gmail.com>
2022-09-15 12:37:57 -0700

    save

--- components/TopBar/BuilderToolbar.tsx
+++ components/TopBar/BuilderToolbar.tsx
@@ -9,7 +9,13 @@ import ColorPicker from './ColorPicker';
 import BrickPicker from './BrickPicker';
 import ToolPicker from './ToolPicker';
 import OptionsModal from '../OptionsScreen/OptionsModal';
+import Svg1x1 from '../Images/1x1svg';
+import Svg1x2 from '../Images/1x2svg';
 import Svg1x3 from '../Images/1x3svg';
+import Svg1x4 from '../Images/1x4svg';
+import Svg2x2 from '../Images/2x2svg';
+import Svg2x3 from '../Images/2x3svg';
+import Svg2x4 from '../Images/2x4svg';
 
 import {
   Provider,
@@ -25,12 +31,18 @@ const BuilderToolbar = props => {
   const {toggleGrid, selectColor, selectedColor, selectBrick, selectedBrick} =
     props;
 
+  console.log('Koca: selectedBrick ', selectedBrick);
+
   const [optionsModalVisible, setOptionsModalVisible] = useState(false);
   const [colorPickerVisible, setColorPickerVisible] = useState(false);
   const [brickPickerVisible, setBrickPickerVisible] = useState(false);
   const [toolPickerVisible, setToolPickerVisible] = useState(false);
 
   const animation = new Animated.Value(-350);
+
+  // const changeBrickIcon = () => {
+  //   selectedBrick === 0 ?
+  // }
 
   const noColors = () => {
     return brickPickerVisible || optionsModalVisible ? true : false;
@@ -190,7 +202,25 @@ const BuilderToolbar = props => {
             />
 
             <IconButton
-              icon={props => <IconMaterial name="toy-brick" {...props} />}
+              icon={props =>
+                selectedBrick === 0 ? (
+                  <Svg1x1 />
+                ) : selectedBrick === 1 ? (
+                  <Svg1x2 />
+                ) : selectedBrick === 3 ? (
+                  <Svg1x3 />
+                ) : selectedBrick === 5 ? (
+                  <Svg1x4 />
+                ) : selectedBrick === 2 ? (
+                  <Svg2x2 />
+                ) : selectedBrick === 4 ? (
+                  <Svg2x3 />
+                ) : selectedBrick === 6 ? (
+                  <Svg2x4 />
+                ) : (
+                  <Svg1x1 />
+                )
+              }
               onPress={() => toggleBrickPicker()}
               {...props}
             />
